b93104225f1b15c664427ab43a4bb374ef42513e
rencdr <demirerencan@outlook.com>
2024-01-07 23:12:09 +0300

    problem 2 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -5,5 +5,10 @@ SELECT *
 FROM CITY
 WHERE Population > 100000 AND CountryCode = 'USA'; */
 
-
+/* 2.Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
+The CountryCode for America is USA.
+-----------
+SELECT NAME
+FROM CITY
+WHERE CountryCode = 'USA' AND Population > 120000; */
 
