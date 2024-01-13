1667fb94f7dff422b4a9e9a1c2aabd64d72a130c
rencdr <demirerencan@outlook.com>
2024-01-13 22:00:14 +0300

    problem 4 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -25,4 +25,19 @@ WHERE age > 50
 ORDER BY age DESC;
 
 */
+/*4.This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
+SELECT 
+  number,
+  CASE 
+    WHEN number % 2 = 0 THEN number * 8
+    ELSE number * 9
+  END AS res
+FROM multiplication;
+
+*/
+
+/*
+5.
+
+*/
