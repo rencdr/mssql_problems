6547765888a99fb0c41f940069fcb324feeae33c
rencdr <demirerencan@outlook.com>
2024-01-14 23:59:27 +0300

    problem 9 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -63,3 +63,10 @@ select n, n*n as res from square
 SELECT s, UPPER(s) AS res
 FROM makeuppercase;
 */
+/*
+9.Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[aeiouAEIOU]';
+*/
