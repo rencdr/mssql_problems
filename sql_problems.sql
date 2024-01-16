abbb52da6166a1b460a212f907f0d50c66ea46e9
rencdr <demirerencan@outlook.com>
2024-01-16 18:44:07 +0300

    problem 10 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -70,3 +70,13 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP '^[aeiouAEIOU]';
 */
+/*
+10.Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u';
+
+*/
+
