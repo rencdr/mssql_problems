e1ae230ef56d701934d112758258aeeb58f8c9b3
rencdr <demirerencan@outlook.com>
2024-01-17 20:15:56 +0300

    problem 13 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -96,4 +96,15 @@ SELECT n,
        CASE WHEN n < 0 OR m < 0 THEN 0 ELSE n * m END AS res
 FROM paperwork;
 */
+/*
+13.You will be given a table numbers, with one column number.
+Return a dataset with two columns: number and is_even, where number contains the original input value, and is_even containing "Even" or "Odd" depending on number column values.
+
+SELECT
+    number,
+    CASE WHEN number % 2 = 0 THEN 'Even' ELSE 'Odd' END AS is_even
+FROM
+    numbers;
+
+*/
 
