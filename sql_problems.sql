6fadd8e93ed0e67f4be1cb0971b2476804510a3c
rencdr <demirerencan@outlook.com>
2024-01-19 19:54:56 +0300

    problem 14 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -107,4 +107,16 @@ FROM
     numbers;
 
 */
+/*
+14.You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+participants table schema
+name (string)
+age (integer)
+attending (boolean)
+
+INSERT INTO participants 
+VALUES ('Can', 28, true);
+SELECT * FROM participants;
+*/
+
 
