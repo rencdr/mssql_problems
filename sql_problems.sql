b9ba7076c7fc87f79fcdc09ab21f8b6903025058
rencdr <demirerencan@outlook.com>
2024-01-21 00:16:04 +0300

    problem 18 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -163,8 +163,17 @@ SELECT age ,
        COUNT(*) AS people_count
 FROM people
 GROUP BY age;
+*/
+/*
+18.Given the following table 'decimals':
 
+** decimals table schema **
+id
+number1
+number2
+Return a table with one column (mod) which is the output of number1 modulus number2.
 
+SELECT number1 % number2 AS MOD from decimals
 */
 
 
