2dbcba36996c65e40c427cd3f1ed70976f2d9a3c
rencdr <demirerencan@outlook.com>
2024-01-21 00:12:12 +0300

    problem 17 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -148,5 +148,23 @@ FROM people
 LEFT JOIN toys ON people.id = toys.people_id
 GROUP BY people.id;
 */
+/*
+17.For this challenge you need to create a simple GROUP BY statement, you want to group all the people by their age and count the people who have the same age.
+
+people table schema
+id
+name
+age
+select table schema
+age [group by]
+people_count (people count)
+
+SELECT age ,
+       COUNT(*) AS people_count
+FROM people
+GROUP BY age;
+
+
+*/
 
 
