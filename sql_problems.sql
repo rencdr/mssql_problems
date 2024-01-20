3271184a0fbe648af065a77e9145716662249969
rencdr <demirerencan@outlook.com>
2024-01-21 00:06:22 +0300

    problem 16 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -129,5 +129,24 @@ SELECT
 FROM 
     demographics;
 */
+/*
+16.For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys
+
+people table schema
+id
+name
+toys table schema
+id
+name
+people_id
+You should return all people fields as well as the toy count as "toy_count".
+
+SELECT people.id,
+       people.name,
+       COUNT(toys.id) AS toy_count
+FROM people
+LEFT JOIN toys ON people.id = toys.people_id
+GROUP BY people.id;
+*/
 
 
