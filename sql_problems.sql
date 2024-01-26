1d6e2a8dfeb700ceb3787ec4dd1844d355493d84
rencdr <demirerencan@outlook.com>
2024-01-26 23:14:50 +0300

    problem 21 added

--- sql_problems.sql
+++ sql_problems.sql
@@ -193,4 +193,24 @@ select
  log(64, number2) log
 from decimals
 */
+/*
+21.You are given a table named repositories, format as below:
 
+** repositories table schema **
+
+project
+commits
+contributors
+address
+The table shows project names of major cryptocurrencies, their numbers of commits and contributors and also a random donation address ( not linked in any way :) ).
+
+Your job is to remove all numbers in the address column and replace with '!', then return a table in the following format:
+
+SELECT
+  project,
+  commits,
+  contributors,
+  REGEXP_REPLACE(address, '\d', '!', 'g') AS address
+FROM
+  repositories;
+*/
